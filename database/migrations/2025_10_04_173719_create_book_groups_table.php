5e7132b5391b4c5cad60bd26fe34a3255a53adc1
MasterlyMisheirt <thewizardofmich@gmail.com>
2025-10-10 09:22:32 +0100

    Fixed issues with migrations

--- database/migrations/2025_10_04_173719_create_book_groups_table.php
+++ database/migrations/2025_10_04_173719_create_book_groups_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -10,8 +11,12 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::table('book_groups', function (Blueprint $table) {
+        Schema::create('book_groups', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
             $table->string('slug')->unique()->after('name');
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
         });
     }
 
@@ -20,9 +25,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::table('book_groups', function (Blueprint $table) {
-            $table->dropUnique(['slug']);
-            $table->dropColumn('slug');
-        });
+        Schema::dropIfExists('book_groups');
     }
 };
